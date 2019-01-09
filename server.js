c5224ed0d993142256e2a0185b3ea97693b351c5
alexander-io <abharris@pugetsound.edu>
2019-01-09 09:27:57 -0800

    require pixijs lib

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req,res) => {
   res.sendFile(__dirname + '/public/views/index.html')
 })
 
+app.get('/pixi.js', (req,res) => {
+  res.sendFile(__dirname + '/node_modules/pixi.js/dist/pixi.min.js')
+})
+
 app.listen(8080, () => {
   console.log('listening for requests on port 8080')
 })
